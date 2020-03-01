efbc2131facb01a4a4122ceb11e1de14d7cd6294
mpreyes <reyes.madelyn.mr@gmail.com>
2020-02-29 23:25:53 -0600

    initial components

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export interface DashboardProps {
+    
+
+}
+export const Dashboard: React.FunctionComponent<DashboardProps> = () => {
+
+
+        return (
+            <h1> Hey, dashboard here!</h1>
+        )
+
+}
